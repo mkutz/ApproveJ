3fdd4d0949c1866d9c81de3de3db69b82928bf62
Michael Kutz <michael.kutz@rewe-group.com>
2025-04-07 19:20:11 +0200

    Add ObjectPrinter

---
+++ modules/core/src/main/java/org/approvej/print/ObjectPrinter.java
@@ -0,0 +1,93 @@
+package org.approvej.print;
+
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAmount;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import org.jspecify.annotations.NullMarked;
+
+/**
+ * A generic printer for Java {@link Object}s that prints their properties and values one per line.
+ */
+@NullMarked
+public class ObjectPrinter implements Printer<Object> {
+
+  /** A {@link Set} of methods that won't be regarded as property getters. */
+  public static final Set<String> EXCLUDED_METHODS = Set.of("hashCode", "toString", "hash");
+
+  /** A {@link Set} of classes that will be printed directly. */
+  public static final Set<Class<?>> SIMPLE_TYPES =
+      Set.of(Boolean.class, CharSequence.class, Number.class, Temporal.class, TemporalAmount.class);
+
+  private static final String PAIR_FORMAT = "%s=%s";
+
+  @Override
+  public String apply(Object value) {
+    return apply(value, "");
+  }
+
+  private String apply(Object object, String baseIndent) {
+    return switch (object) {
+      case Map<?, ?> map -> applyMap(map, baseIndent);
+      case Collection<?> collection -> applyCollection(collection, baseIndent);
+      default -> applyObject(object, baseIndent);
+    };
+  }
+
+  private String applyCollection(Collection<?> collection, String baseIndent) {
+    String indent = baseIndent + "  ";
+    return collection.stream()
+        .map(element -> apply(element, indent))
+        .collect(
+            joining(
+                ",%n%s".formatted(indent),
+                "[%n%s".formatted(indent),
+                "%n%s]".formatted(baseIndent)));
+  }
+
+  private String applyMap(Map<?, ?> map, String baseIndent) {
+    return applyCollection(
+        new TreeMap<>(map)
+            .entrySet().stream()
+                .map(
+                    entry ->
+                        PAIR_FORMAT.formatted(
+                            entry.getKey(), apply(entry.getValue(), baseIndent + "  ")))
+                .toList(),
+        baseIndent);
+  }
+
+  private String applyObject(Object object, String baseIndent) {
+    String indent = baseIndent + "  ";
+    Class<?> type = object.getClass();
+    if (SIMPLE_TYPES.stream().anyMatch(simpleType -> simpleType.isAssignableFrom(type))) {
+      return object.toString();
+    }
+    return stream(object.getClass().getDeclaredMethods())
+        .filter(method -> !EXCLUDED_METHODS.contains(method.getName()))
+        .filter(method -> method.getParameterCount() == 0 && method.getReturnType() != Void.class)
+        .sorted(Comparator.comparing(Method::getName))
+        .map(
+            method -> {
+              try {
+                return PAIR_FORMAT.formatted(
+                    method.getName(), apply(method.invoke(object), indent));
+              } catch (IllegalAccessException | InvocationTargetException e) {
+                return PAIR_FORMAT.formatted(method.getName(), "<inaccessible>");
+              }
+            })
+        .collect(
+            joining(
+                ",%n%s".formatted(indent),
+                "%s [%n%s".formatted(object.getClass().getSimpleName(), indent),
+                "%n%s]".formatted(baseIndent)));
+  }
+}
---
+++ modules/core/src/test/java/org/approvej/print/ObjectPrinterTest.java
@@ -0,0 +1,166 @@
+package org.approvej.print;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class ObjectPrinterTest {
+
+  ObjectPrinter printer = new ObjectPrinter();
+
+  @Test
+  void apply() {
+    var exampleObject = new SimpleExampleClass("value1", 42, true);
+
+    assertThat(printer.apply(exampleObject))
+        .isEqualTo(
+            """
+            SimpleExampleClass [
+              field1=value1,
+              field2=42,
+              field3=true
+            ]""");
+  }
+
+  private record SimpleExampleClass(String field1, int field2, boolean field3) {}
+
+  @Test
+  void apply_complex_property() {
+    var exampleObject =
+        new ComplexExampleClass("value1", 42, true, new SimpleExampleClass("value2", 24, false));
+
+    assertThat(printer.apply(exampleObject))
+        .isEqualTo(
+            """
+            ComplexExampleClass [
+              field1=value1,
+              field2=42,
+              field3=true,
+              field4=SimpleExampleClass [
+                field1=value2,
+                field2=24,
+                field3=false
+              ]
+            ]""");
+  }
+
+  private record ComplexExampleClass(
+      String field1, int field2, boolean field3, SimpleExampleClass field4) {}
+
+  @Test
+  void apply_list() {
+    assertThat(printer.apply(List.of("a", "b", "c")))
+        .isEqualTo(
+            """
+            [
+              a,
+              b,
+              c
+            ]""");
+  }
+
+  @Test
+  void apply_nested_lists() {
+    assertThat(printer.apply(List.of("a", List.of(1, 2, 3), "c")))
+        .isEqualTo(
+            """
+            [
+              a,
+              [
+                1,
+                2,
+                3
+              ],
+              c
+            ]""");
+  }
+
+  @Test
+  void apply_map() {
+    assertThat(printer.apply(Map.of("a", 1, "b", 2, "c", 3)))
+        .isEqualTo(
+            """
+            [
+              a=1,
+              b=2,
+              c=3
+            ]""");
+  }
+
+  @Test
+  void apply_map_of_complex() {
+    assertThat(
+            printer.apply(Map.of("a", new SimpleExampleClass("b", 2, true), "b", "hello", "c", 3)))
+        .isEqualTo(
+            """
+            [
+              a=SimpleExampleClass [
+                field1=b,
+                field2=2,
+                field3=true
+              ],
+              b=hello,
+              c=3
+            ]""");
+  }
+
+  @Test
+  void apply_collection_property() {
+    var value = new ComplexExampleCollectionClass("value1", List.of("a", "b", "c"));
+
+    assertThat(printer.apply(value))
+        .isEqualTo(
+            """
+            ComplexExampleCollectionClass [
+              field1=value1,
+              field2=[
+                a,
+                b,
+                c
+              ]
+            ]""");
+  }
+
+  private record ComplexExampleCollectionClass(String field1, List<String> field2) {}
+
+  @Test
+  void apply_collection_of_complex() {
+    var value =
+        List.of(
+            new ComplexExampleClass(
+                "value1", 42, true, new SimpleExampleClass("value2", 24, false)));
+
+    assertThat(printer.apply(value))
+        .isEqualTo(
+            """
+            [
+              ComplexExampleClass [
+                field1=value1,
+                field2=42,
+                field3=true,
+                field4=SimpleExampleClass [
+                  field1=value2,
+                  field2=24,
+                  field3=false
+                ]
+              ]
+            ]""");
+  }
+
+  @Test
+  void apply_temporal() {
+    var value = List.of(LocalDateTime.of(2345, 6, 7, 8, 9, 10), Duration.ofHours(1));
+
+    assertThat(printer.apply(value))
+        .isEqualTo(
+            """
+            [
+              2345-06-07T08:09:10,
+              PT1H
+            ]""");
+  }
+}
