f7876f91c24def7b9ea8f82c174d73bb5acf0f98
Michael Kutz <michael.kutz@rewe-group.com>
2025-04-10 15:37:42 +0200

    Fix flaky test

--- modules/core/src/test/java/org/approvej/scrub/InstantScrubberTest.java
+++ modules/core/src/test/java/org/approvej/scrub/InstantScrubberTest.java
@@ -4,6 +4,7 @@ import static java.time.format.DateTimeFormatter.ISO_INSTANT;
 import static org.approvej.scrub.InstantScrubber.instants;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Instant;
 import org.junit.jupiter.api.Test;
 
 class InstantScrubberTest {
@@ -11,7 +12,7 @@ class InstantScrubberTest {
   @Test
   void apply() {
     var scrubber = instants(ISO_INSTANT);
-    assertThat(scrubber.apply("datetime: 2025-04-10T11:48:04.459229Z"))
+    assertThat(scrubber.apply("datetime: %s".formatted(Instant.now())))
         .isEqualTo("datetime: [instant 1]");
   }
 }
