c0faba0412217390db8c01b455e00984d1246104
Michael Kutz <michael.kutz@rewe-group.com>
2025-06-03 13:35:32 +0200

    Remove deprecated accept method from Approver

--- modules/core/src/main/java/org/approvej/approve/Approver.java
+++ modules/core/src/main/java/org/approvej/approve/Approver.java
@@ -8,19 +8,4 @@ import org.approvej.ApprovalResult;
  * A {@link Consumer} that approved a printed value and (optionally) stores the value in some
  * fashion if approved for a later execution.
  */
-public interface Approver extends Consumer<String>, Function<String, ApprovalResult> {
-
-  /**
-   * Approves a received value against a previously approved value.
-   *
-   * @param received the received value
-   * @throws org.approvej.ApprovalError if the received value was not approved
-   * @deprecated use {@link #apply(Object)} to get an {@link ApprovalResult} and (optionally) call
-   *     {@link ApprovalResult#throwIfNotApproved()} to achieve the same outcome.
-   */
-  @Override
-  @Deprecated(since = "0.8.6", forRemoval = true)
-  default void accept(String received) {
-    apply(received).throwIfNotApproved();
-  }
-}
+public interface Approver extends Function<String, ApprovalResult> {}
