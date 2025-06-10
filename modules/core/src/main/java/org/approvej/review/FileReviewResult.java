d7a09db3113c11adac83f8c149da7fa227da3394
Michael Kutz <michael.kutz@rewe-group.com>
2025-06-10 17:05:50 +0200

    Add missing JavaDoc

--- modules/core/src/main/java/org/approvej/review/FileReviewResult.java
+++ modules/core/src/main/java/org/approvej/review/FileReviewResult.java
@@ -1,3 +1,9 @@
 package org.approvej.review;
 
+/**
+ * Result class for {@link FileReviewer}s.
+ *
+ * @param needsReapproval indicates if the result should be reapproved after the review (e.g.
+ *     because the approval file was modified)
+ */
 public record FileReviewResult(boolean needsReapproval) implements ReviewResult {}
