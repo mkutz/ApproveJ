cc716c8fb19b5cab5030ac3f145ffa29a09db13e
Michael Kutz <michael.kutz@rewe-group.com>
2025-08-12 14:32:35 +0200

    Fix handling of deeper JsonPointers
    Resolves #99

---
+++ modules/json-jackson/src/main/java/org/approvej/json/jackson/JsonPointerScrubberException.java
@@ -0,0 +1,20 @@
+package org.approvej.json.jackson;
+
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+/** Exception thrown when scrubbing a JSON node defined by {@link JsonPointer} fails. */
+public class JsonPointerScrubberException extends RuntimeException {
+
+  /**
+   * Creates a new JsonPointerScrubberException.
+   *
+   * @param jsonPointer the full pointer that should have been scrubbed
+   * @param parentNode the actually found parent node (if any)
+   */
+  public JsonPointerScrubberException(JsonPointer jsonPointer, JsonNode parentNode) {
+    super(
+        "Failed to scrub JSON pointer %s, parent node %s"
+            .formatted(jsonPointer.toString(), parentNode));
+  }
+}
