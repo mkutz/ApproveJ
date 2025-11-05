b0d61a0381165023cd9862d90e15e4781498cbaf
Michael Kutz <michael.kutz@rewe-group.com>
2025-11-05 16:36:42 +0100

    Make ToStringPrinter a record

--- modules/core/src/main/java/org/approvej/print/ToStringPrinter.java
+++ modules/core/src/main/java/org/approvej/print/ToStringPrinter.java
@@ -5,17 +5,7 @@ import org.jspecify.annotations.Nullable;
 
 /** A simple {@link Printer} implementation that uses the {@link Object#toString()} method. */
 @NullMarked
-public class ToStringPrinter implements Printer<Object> {
-
-  /**
-   * Creates a new {@link ToStringPrinter}.
-   *
-   * <p>This constructor is public to allow instantiation via reflection, e.g. in the {@link
-   * org.approvej.Configuration} class.
-   */
-  public ToStringPrinter() {
-    // No initialization needed
-  }
+public record ToStringPrinter() implements Printer<Object> {
 
   @Override
   public String apply(@Nullable Object value) {
