7113fa533460547e63a16c17592a923a73b9f5b9
Michael Kutz <michael.kutz@rewe-group.com>
2025-11-10 19:55:06 +0100

    Remove public from test classes

--- modules/core/src/test/java/org/approvej/scrub/LabelReplacementTest.java
+++ modules/core/src/test/java/org/approvej/scrub/LabelReplacementTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class LabelReplacementTest {
+class LabelReplacementTest {
 
   @Test
   void apply() {
--- modules/core/src/test/java/org/approvej/scrub/NumberedReplacementTest.java
+++ modules/core/src/test/java/org/approvej/scrub/NumberedReplacementTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class NumberedReplacementTest {
+class NumberedReplacementTest {
 
   @Test
   void apply() {
