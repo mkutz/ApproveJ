42b16c237a145e9217df3711fbe1708bb3759257
Michael Kutz <michael.kutz@rewe-group.com>
2025-11-17 09:19:03 +0100

    Turn built-in Printers into records

--- modules/core/src/main/java/org/approvej/print/MultiLineStringPrintFormat.java
+++ modules/core/src/main/java/org/approvej/print/MultiLineStringPrintFormat.java
@@ -1,32 +1,30 @@
 package org.approvej.print;
 
-import org.approvej.Configuration;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAmount;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.UUID;
+import java.util.stream.Stream;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 /**
  * A generic printer for Java {@link Object}s that prints their properties and values one per line.
  */
 @NullMarked
-public final class MultiLineStringPrintFormat implements PrintFormat<Object> {
-
-  private final MultiLineStringPrinter<Object> printer;
-
-  /**
-   * @param printer the printer implementing this format
-   */
-  MultiLineStringPrintFormat(MultiLineStringPrinter<Object> printer) {
-    this.printer = printer;
-  }
-
-  /** Default constructor to be used in {@link Configuration}. */
-  public MultiLineStringPrintFormat() {
-    this(new MultiLineStringPrinter<>());
-  }
-
-  @Override
-  public Printer<Object> printer() {
-    return printer;
-  }
+public record MultiLineStringPrintFormat(Printer<Object> printer) implements PrintFormat<Object> {
 
   /**
    * Sort the printed object's fields alphabetically by their name. By default, the fields will be
@@ -35,7 +33,8 @@ public final class MultiLineStringPrintFormat implements PrintFormat<Object> {
    * @return a copy of this sorting fields by name
    */
   public MultiLineStringPrintFormat sorted() {
-    return new MultiLineStringPrintFormat(printer.sorted());
+    return new MultiLineStringPrintFormat(
+        new MultiLineStringPrinter<>(Comparator.comparing(Field::getName)));
   }
 
   /**
@@ -44,6 +43,152 @@ public final class MultiLineStringPrintFormat implements PrintFormat<Object> {
    * @return a new {@link MultiLineStringPrintFormat}
    */
   public static MultiLineStringPrintFormat multiLineString() {
-    return new MultiLineStringPrintFormat();
+    return new MultiLineStringPrintFormat(new MultiLineStringPrinter<>((field1, field2) -> 0));
+  }
+
+  /**
+   * A generic printer for Java {@link Object}s that prints their properties and values one per
+   * line.
+   *
+   * @param fieldComparator a {@link Comparator} used to sort the printed value's {@link Field}s
+   * @param <T> the type of the object to be printed
+   */
+  @NullMarked
+  private record MultiLineStringPrinter<T>(Comparator<Field> fieldComparator)
+      implements Printer<T> {
+
+    /** A {@link Set} of classes that will be printed directly. */
+    private static final Set<Class<?>> SIMPLE_TYPES =
+        Set.of(
+            Boolean.class,
+            Character.class,
+            CharSequence.class,
+            Class.class,
+            Enum.class,
+            Number.class,
+            UUID.class,
+            Temporal.class,
+            TemporalAmount.class);
+
+    private static final String PAIR_FORMAT = "%s=%s";
+
+    @Override
+    public String apply(Object value) {
+      return apply(value, "");
+    }
+
+    private String apply(@Nullable Object object, String baseIndent) {
+      return switch (object) {
+        case Map<?, ?> map -> applyMap(map, baseIndent);
+        case Collection<?> collection -> applyCollection(collection, baseIndent);
+        case null -> applyObject("null", baseIndent);
+        default -> applyObject(object, baseIndent);
+      };
+    }
+
+    private String applyCollection(Collection<?> collection, String baseIndent) {
+      if (collection.isEmpty()) {
+        return "[]";
+      }
+      String indent = baseIndent + "  ";
+      return collection.stream()
+          .map(element -> apply(element, indent))
+          .collect(
+              joining(
+                  ",%n%s".formatted(indent),
+                  "[%n%s".formatted(indent),
+                  "%n%s]".formatted(baseIndent)));
+    }
+
+    private String applyMap(Map<?, ?> map, String baseIndent) {
+      return applyCollection(
+          new TreeMap<>(map)
+              .entrySet().stream()
+                  .map(
+                      entry ->
+                          PAIR_FORMAT.formatted(
+                              entry.getKey(), apply(entry.getValue(), baseIndent + "  ")))
+                  .toList(),
+          baseIndent);
+    }
+
+    private String applyObject(@Nullable Object object, String baseIndent) {
+      String indent = baseIndent + "  ";
+      if (object == null
+          || SIMPLE_TYPES.stream()
+              .anyMatch(simpleType -> simpleType.isAssignableFrom(object.getClass()))) {
+        return "%s".formatted(object);
+      }
+      return getFieldsAndGetters(object)
+          .filter(FieldAndGetter::hasGetter)
+          .map(
+              fieldAndGetter ->
+                  PAIR_FORMAT.formatted(
+                      fieldAndGetter.field.getName(), apply(fieldAndGetter.value(), indent)))
+          .collect(
+              joining(
+                  ",%n%s".formatted(indent),
+                  "%s [%n%s".formatted(object.getClass().getSimpleName(), indent),
+                  "%n%s]".formatted(baseIndent)));
+    }
+
+    private Stream<FieldAndGetter> getFieldsAndGetters(Object object) {
+      return getFields(object).map(field -> new FieldAndGetter(field, object));
+    }
+
+    private Stream<Field> getFields(Object object) {
+      return getTypes(object)
+          .flatMap(aClass -> stream(aClass.getDeclaredFields()))
+          .filter(field -> !Modifier.isStatic(field.getModifiers()))
+          .sorted(fieldComparator);
+    }
+
+    private Stream<Class<?>> getTypes(Object object) {
+      return Stream.<Class<?>>iterate(
+          object.getClass(), type -> type != Object.class, Class::getSuperclass)
+          .toList()
+          .reversed()
+          .stream();
+    }
+
+    private record FieldAndGetter(Object object, Field field, Optional<Method> getter) {
+
+      private FieldAndGetter(Field field, Object object) {
+        this(
+            object,
+            field,
+            stream(field.getDeclaringClass().getDeclaredMethods())
+                .filter(
+                    method ->
+                        method.getParameterCount() == 0
+                            && Void.class != method.getReturnType()
+                            && method
+                                .getName()
+                                .matches(
+                                    String.format(
+                                        "(%s|get%2$s|is%2$s)",
+                                        field.getName(),
+                                        field.getName().substring(0, 1).toUpperCase()
+                                            + field.getName().substring(1))))
+                .findFirst());
+      }
+
+      boolean hasGetter() {
+        return getter.isPresent();
+      }
+
+      @Nullable Object value() {
+        return getter
+            .map(
+                method -> {
+                  try {
+                    return method.invoke(object);
+                  } catch (IllegalAccessException | InvocationTargetException e) {
+                    return "<inaccessible>";
+                  }
+                })
+            .orElse(null);
+      }
+    }
   }
 }
