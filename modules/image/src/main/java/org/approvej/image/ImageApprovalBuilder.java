93d145aa54866528ee227c9ff92a15b5ce1fde9d
Michael Kutz <michael.kutz@rewe-group.com>
2025-11-18 11:51:14 +0100

    Make reviewing work with images

--- modules/image/src/main/java/org/approvej/image/ImageApprovalBuilder.java
+++ modules/image/src/main/java/org/approvej/image/ImageApprovalBuilder.java
@@ -1,5 +1,6 @@
 package org.approvej.image;
 
+import static org.approvej.Configuration.configuration;
 import static org.approvej.image.approve.ImageFileApprover.imageFile;
 
 import java.awt.*;
@@ -7,7 +8,10 @@ import java.awt.image.BufferedImage;
 import org.approvej.ApprovalBuilder;
 import org.approvej.approve.PathProvider;
 import org.approvej.image.approve.ImageFileApprover;
+import org.approvej.review.FileReviewer;
+import org.approvej.review.ReviewResult;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 @NullMarked
 public class ImageApprovalBuilder {
@@ -15,11 +19,17 @@ public class ImageApprovalBuilder {
   private final BufferedImage value;
   private final String name;
   private final String filenameExtension;
+  @Nullable private final FileReviewer fileReviewer;
 
-  private ImageApprovalBuilder(BufferedImage image, String name, String filenameExtension) {
+  private ImageApprovalBuilder(
+      BufferedImage image,
+      String name,
+      String filenameExtension,
+      @Nullable FileReviewer fileReviewer) {
     this.value = image;
     this.name = name;
     this.filenameExtension = filenameExtension;
+    this.fileReviewer = fileReviewer;
   }
 
   /**
@@ -29,7 +39,7 @@ public class ImageApprovalBuilder {
    * @return a new {@link ApprovalBuilder} for the given value
    */
   public static ImageApprovalBuilder approveImage(BufferedImage value) {
-    return new ImageApprovalBuilder(value, "", "png");
+    return new ImageApprovalBuilder(value, "", "png", configuration.defaultFileReviewer());
   }
 
   public void byFile(PathProvider pathProvider) {
@@ -37,6 +47,12 @@ public class ImageApprovalBuilder {
         pathProvider.filenameAffix(name).filenameExtension(filenameExtension);
     ImageFileApprover approver = imageFile(updatedPathProvider);
     ImageApprovalResult approvalResult = approver.apply(value);
+    if (approvalResult.needsApproval() && fileReviewer != null) {
+      ReviewResult reviewResult = fileReviewer.apply(updatedPathProvider);
+      if (reviewResult.needsReapproval()) {
+        approvalResult = approver.apply(value);
+      }
+    }
     approvalResult.throwIfNotApproved();
   }
 }
